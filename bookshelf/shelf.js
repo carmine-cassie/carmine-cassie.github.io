df4698acafd78bbc037edd627e6dec2657a62d36
carmine-cassie <carmine-cassie@users.noreply.github.com>
2026-05-15 11:43:05 +0000

    deploy: e81528a7d840445b8360ea8f0180c7cf9b1b7a7c

---
+++ bookshelf/shelf.js
@@ -0,0 +1,33 @@
+const shelves = {
+  "May 2026": [
+    {
+      series: "Animorphs",
+      entry: "1",
+      title: "The Invasion",
+      author: "K.A. Applegate",
+      words: 34551,
+      size: "small",
+    },
+    {
+      series: "This Used to be <br> About Dungeons",
+      entry: "1",
+      author: "Alexander Wales",
+      words: 204736,
+      size: "large",
+    },
+    {
+      title: "The Disaster Artist",
+      author: "Greg Sestero & Tom Bissell",
+      words: 76697,
+      size: "medium",
+    },
+    {
+      series: "Arc of a Scythe",
+      entry: "1",
+      title: "Scythe",
+      author: "Neal Shusterman",
+      words: 94933,
+      size: "medium",
+    },
+  ]
+}
---
+++ bookshelf/wildbow.js
@@ -0,0 +1,983 @@
+const shelves = {
+  "worm": [
+    {
+      series: "Worm",
+      entry: "1",
+      title: "Gestation",
+      author: "Wildbow",
+      words: 17471,
+      size: "small"
+    },
+    {
+      series: "Worm",
+      entry: "2",
+      title: "Insinuation",
+      author: "Wildbow",
+      words: 25411,
+      size: "small"
+    },
+    {
+      series: "Worm",
+      entry: "3",
+      title: "Agitation",
+      author: "Wildbow",
+      words: 37850,
+      size: "small"
+    },
+    {
+      series: "Worm",
+      entry: "4",
+      title: "Shell",
+      author: "Wildbow",
+      words: 37474,
+      size: "small"
+    },
+    {
+      series: "Worm",
+      entry: "5",
+      title: "Hive",
+      author: "Wildbow",
+      words: 37369,
+      size: "small"
+    },
+    {
+      series: "Worm",
+      entry: "6",
+      title: "Tangle",
+      author: "Wildbow",
+      words: 44139,
+      size: "medium"
+    },
+    {
+      series: "Worm",
+      entry: "7",
+      title: "Buzz",
+      author: "Wildbow",
+      words: 47145,
+      size: "medium"
+    },
+    {
+      series: "Worm",
+      entry: "8",
+      title: "Extermination",
+      author: "Wildbow",
+      words: 51703,
+      size: "medium"
+    },
+    {
+      series: "Worm",
+      entry: "9",
+      title: "Sentinel",
+      author: "Wildbow",
+      words: 34600,
+      size: "small"
+    },
+    {
+      series: "Worm",
+      entry: "10",
+      title: "Parasite",
+      author: "Wildbow",
+      words: 38655,
+      size: "small"
+    },
+    {
+      series: "Worm",
+      entry: "11",
+      title: "Infestation",
+      author: "Wildbow",
+      words: 83401,
+      size: "medium"
+    },
+    {
+      series: "Worm",
+      entry: "12",
+      title: "Plague",
+      author: "Wildbow",
+      words: 57527,
+      size: "medium"
+    },
+    {
+      series: "Worm",
+      entry: "13",
+      title: "Snare",
+      author: "Wildbow",
+      words: 60541,
+      size: "medium"
+    },
+    {
+      series: "Worm",
+      entry: "14",
+      title: "Prey",
+      author: "Wildbow",
+      words: 73498,
+      size: "medium"
+    },
+    {
+      series: "Worm",
+      entry: "15",
+      title: "Colony",
+      author: "Wildbow",
+      words: 78154,
+      size: "medium"
+    },
+    {
+      series: "Worm",
+      entry: "16",
+      title: "Monarch",
+      author: "Wildbow",
+      words: 91947,
+      size: "medium"
+    },
+    {
+      series: "Worm",
+      entry: "17",
+      title: "Migration",
+      author: "Wildbow",
+      words: 52506,
+      size: "medium"
+    },
+    {
+      series: "Worm",
+      entry: "18",
+      title: "Queen",
+      author: "Wildbow",
+      words: 77070,
+      size: "medium"
+    },
+    {
+      series: "Worm",
+      entry: "19",
+      title: "Scourge",
+      author: "Wildbow",
+      words: 66684,
+      size: "medium"
+    },
+    {
+      series: "Worm",
+      entry: "20",
+      title: "Chrysalis",
+      author: "Wildbow",
+      words: 43508,
+      size: "medium"
+    },
+    {
+      series: "Worm",
+      entry: "21",
+      title: "Imago",
+      author: "Wildbow",
+      words: 56560,
+      size: "medium"
+    },
+    {
+      series: "Worm",
+      entry: "22",
+      title: "Cell",
+      author: "Wildbow",
+      words: 58640,
+      size: "medium"
+    },
+    {
+      series: "Worm",
+      entry: "23",
+      title: "Drone",
+      author: "Wildbow",
+      words: 45308,
+      size: "medium"
+    },
+    {
+      series: "Worm",
+      entry: "24",
+      title: "Crushed",
+      author: "Wildbow",
+      words: 56206,
+      size: "medium"
+    },
+    {
+      series: "Worm",
+      entry: "25",
+      title: "Scarab",
+      author: "Wildbow",
+      words: 55233,
+      size: "medium"
+    },
+    {
+      series: "Worm",
+      entry: "26",
+      title: "Sting",
+      author: "Wildbow",
+      words: 85143,
+      size: "medium"
+    },
+    {
+      series: "Worm",
+      entry: "27",
+      title: "Extinction",
+      author: "Wildbow",
+      words: 44723,
+      size: "medium"
+    },
+    {
+      series: "Worm",
+      entry: "28",
+      title: "Cockroaches",
+      author: "Wildbow",
+      words: 49838,
+      size: "medium"
+    },
+    {
+      series: "Worm",
+      entry: "29",
+      title: "Venom",
+      author: "Wildbow",
+      words: 72542,
+      size: "medium"
+    },
+    {
+      series: "Worm",
+      entry: "30",
+      title: "Speck",
+      author: "Wildbow",
+      words: 54841,
+      size: "medium"
+    },
+    {
+      series: "Worm",
+      entry: "e",
+      title: "Teneral",
+      author: "Wildbow",
+      words: 46564,
+      size: "medium"
+    },
+  ],
+  "pact": [
+    {
+      series: "Pact",
+      entry: "1",
+      title: "Bonds",
+      author: "Wildbow",
+      words: 56276,
+      size: "medium"
+    },
+    {
+      series: "Pact",
+      entry: "2",
+      title: "Damages",
+      author: "Wildbow",
+      words: 62825,
+      size: "medium"
+    },
+    {
+      series: "Pact",
+      entry: "3",
+      title: "Breach",
+      author: "Wildbow",
+      words: 42660,
+      size: "small"
+    },
+    {
+      series: "Pact",
+      entry: "4",
+      title: "Collateral",
+      author: "Wildbow",
+      words: 92742,
+      size: "medium"
+    },
+
+    {
+      series: "Pact",
+      entry: "5",
+      title: "Conviction",
+      author: "Wildbow",
+      words: 48397,
+      size: "small"
+    },
+    {
+      series: "Pact",
+      entry: "6",
+      title: "Subordination",
+      author: "Wildbow",
+      words: 76020,
+      size: "medium"
+    },
+    {
+      series: "Pact",
+      entry: "7",
+      title: "Void",
+      author: "Wildbow",
+      words: 70634,
+      size: "medium"
+    },
+    {
+      series: "Pact",
+      entry: "8",
+      title: "Signature",
+      author: "Wildbow",
+      words: 48204,
+      size: "small"
+    },
+    {
+      series: "Pact",
+      entry: "9",
+      title: "Null",
+      author: "Wildbow",
+      words: 51150,
+      size: "medium"
+    },
+    {
+      series: "Pact",
+      entry: "10",
+      title: "Mala Fide",
+      author: "Wildbow",
+      words: 49151,
+      size: "small"
+    },
+    {
+      series: "Pact",
+      entry: "11",
+      title: "Malfeasance",
+      author: "Wildbow",
+      words: 70244,
+      size: "medium"
+    },
+    {
+      series: "Pact",
+      entry: "12",
+      title: "Duress",
+      author: "Wildbow",
+      words: 55750,
+      size: "medium"
+    },
+    {
+      series: "Pact",
+      entry: "13",
+      title: "Execution",
+      author: "Wildbow",
+      words: 54191,
+      size: "medium"
+    },
+    {
+      series: "Pact",
+      entry: "14",
+      title: "Sine Die",
+      author: "Wildbow",
+      words: 55361,
+      size: "medium"
+    },
+    {
+      series: "Pact",
+      entry: "15",
+      title: "Posession",
+      author: "Wildbow",
+      words: 43818,
+      size: "small"
+    },
+    {
+      series: "Pact",
+      entry: "16",
+      title: "Judgement",
+      author: "Wildbow",
+      words: 70212,
+      size: "medium"
+    },
+  ],
+  "twig": [
+    {
+      series: "Twig",
+      entry: "1",
+      title: "Taking Root",
+      author: "Wildbow",
+      words: 69033,
+      size: "medium"
+    },
+    {
+      series: "Twig",
+      entry: "2",
+      title: "Cat out of the Bag",
+      author: "Wildbow",
+      words: 52159,
+      size: "small"
+    },
+    {
+      series: "Twig",
+      entry: "3",
+      title: "Lips Sealed",
+      author: "Wildbow",
+      words: 52195,
+      size: "small"
+    },
+    {
+      series: "Twig",
+      entry: "4",
+      title: "Sitch in Time",
+      author: "Wildbow",
+      words: 59037,
+      size: "small"
+    },
+    {
+      series: "Twig",
+      entry: "5",
+      title: "Esprit de Corpse",
+      author: "Wildbow",
+      words: 75657,
+      size: "medium"
+    },
+    {
+      series: "Twig",
+      entry: "6",
+      title: "Lamb to the Slaughter",
+      author: "Wildbow",
+      words: 68833,
+      size: "medium"
+    },
+    {
+      series: "Twig",
+      entry: "7",
+      title: "Tooth and Nail",
+      author: "Wildbow",
+      words: 84199,
+      size: "medium"
+    },
+    {
+      series: "Twig",
+      entry: "8",
+      title: "Bleeding Edge",
+      author: "Wildbow",
+      words: 80270,
+      size: "medium"
+    },
+    {
+      series: "Twig",
+      entry: "9",
+      title: "Counting Sheep",
+      author: "Wildbow",
+      words: 89320,
+      size: "medium"
+    },
+    {
+      series: "Twig",
+      entry: "10",
+      title: "In Sheep's Clothing",
+      author: "Wildbow",
+      words: 109523,
+      size: "large"
+    },
+    {
+      series: "Twig",
+      entry: "11",
+      title: "Cut to the Quick",
+      author: "Wildbow",
+      words: 82427,
+      size: "medium"
+    },
+    {
+      series: "Twig",
+      entry: "12",
+      title: "Dyed in the Wool",
+      author: "Wildbow",
+      words: 58521,
+      size: "small"
+    },
+    {
+      series: "Twig",
+      entry: "13",
+      title: "Black Sheep",
+      author: "Wildbow",
+      words: 70068,
+      size: "medium"
+    },
+    {
+      series: "Twig",
+      entry: "14",
+      title: "Thicker than Water",
+      author: "Wildbow",
+      words: 99213,
+      size: "medium"
+    },
+    {
+      series: "Twig",
+      entry: "15",
+      title: "Bitter Pill",
+      author: "Wildbow",
+      words: 85745,
+      size: "medium"
+    },
+    {
+      series: "Twig",
+      entry: "16",
+      title: "Head over Heels",
+      author: "Wildbow",
+      words: 73635,
+      size: "medium"
+    },
+    {
+      series: "Twig",
+      entry: "17",
+      title: "Gut Feeling",
+      author: "Wildbow",
+      words: 86971,
+      size: "medium"
+    },
+    {
+      series: "Twig",
+      entry: "18",
+      title: "Dog Eat Dog",
+      author: "Wildbow",
+      words: 97817,
+      size: "medium"
+    },
+    {
+      series: "Twig",
+      entry: "19",
+      title: "Root and Branch",
+      author: "Wildbow",
+      words: 86372,
+      size: "medium"
+    },
+    {
+      series: "Twig",
+      entry: "20",
+      title: "Crown of Thorns",
+      author: "Wildbow",
+      words: 102027,
+      size: "large"
+    },
+    {
+      series: "Twig",
+      entry: "e",
+      title: "Forest for the Trees",
+      author: "Wildbow",
+      words: 22451,
+      size: "small"
+    },
+  ],
+  "ward": [
+    {
+      series: "Ward",
+      entry: "0",
+      title: "Glow-worm",
+      author: "Wildbow",
+      words: 26127,
+      size: "small"
+    },
+    {
+      series: "Ward",
+      entry: "1",
+      title: "Daybreak",
+      author: "Wildbow",
+      words: 51622,
+      size: "medium"
+    },
+    {
+      series: "Ward",
+      entry: "2",
+      title: "Flare",
+      author: "Wildbow",
+      words: 58786,
+      size: "medium"
+    },
+    {
+      series: "Ward",
+      entry: "3",
+      title: "Glare",
+      author: "Wildbow",
+      words: 47502,
+      size: "small"
+    },
+    {
+      series: "Ward",
+      entry: "4",
+      title: "Shade",
+      author: "Wildbow",
+      words: 83477,
+      size: "medium"
+    },
+    {
+      series: "Ward",
+      entry: "5",
+      title: "Shadow",
+      author: "Wildbow",
+      words: 107792,
+      size: "large"
+    },
+    {
+      series: "Ward",
+      entry: "6",
+      title: "Pitch",
+      author: "Wildbow",
+      words: 55725,
+      size: "medium"
+    },
+    {
+      series: "Ward",
+      entry: "7",
+      title: "Torch",
+      author: "Wildbow",
+      words: 82197,
+      size: "medium"
+    },
+    {
+      series: "Ward",
+      entry: "X",
+      title: "Eclipse",
+      author: "Wildbow",
+      words: 52767,
+      size: "medium"
+    },
+    {
+      series: "Ward",
+      entry: "8",
+      title: "Beacon",
+      author: "Wildbow",
+      words: 82335,
+      size: "medium"
+    },
+    {
+      series: "Ward",
+      entry: "9",
+      title: "Gleaming",
+      author: "Wildbow",
+      words: 139878,
+      size: "large"
+    },
+    {
+      series: "Ward",
+      entry: "10",
+      title: "Polarize",
+      author: "Wildbow",
+      words: 102487,
+      size: "medium"
+    },
+    {
+      series: "Ward",
+      entry: "11",
+      title: "Blinding",
+      author: "Wildbow",
+      words: 108911,
+      size: "large"
+    },
+    {
+      series: "Ward",
+      entry: "12",
+      title: "Heavens",
+      author: "Wildbow",
+      words: 124026,
+      size: "large"
+    },
+    {
+      series: "Ward",
+      entry: "13",
+      title: "Black",
+      author: "Wildbow",
+      words: 101174,
+      size: "medium"
+    },
+    {
+      series: "Ward",
+      entry: "14",
+      title: "Breaking",
+      author: "Wildbow",
+      words: 90271,
+      size: "medium"
+    },
+    {
+      series: "Ward",
+      entry: "15",
+      title: "Dying",
+      author: "Wildbow",
+      words: 90965,
+      size: "medium"
+    },
+    {
+      series: "Ward",
+      entry: "16",
+      title: "From Within",
+      author: "Wildbow",
+      words: 102695,
+      size: "medium"
+    },
+    {
+      series: "Ward",
+      entry: "17",
+      title: "Sundown",
+      author: "Wildbow",
+      words: 109981,
+      size: "large"
+    },
+    {
+      series: "Ward",
+      entry: "18",
+      title: "Radiation",
+      author: "Wildbow",
+      words: 85096,
+      size: "medium"
+    },
+    {
+      series: "Ward",
+      entry: "19",
+      title: "Infrared",
+      author: "Wildbow",
+      words: 124579,
+      size: "large"
+    },
+    {
+      series: "Ward",
+      entry: "20",
+      title: "Last",
+      author: "Wildbow",
+      words: 116391,
+      size: "large"
+    },
+  ],
+  "pale": [
+    {
+      series: "Pale",
+      entry: "1",
+      title: "Lost for Words",
+      author: "Wildbow",
+      words: 7174+82584,
+      size: "medium"
+    },
+    {
+      series: "Pale",
+      entry: "2",
+      title: "Stolen Away",
+      author: "Wildbow",
+      words: 116675,
+      size: "medium"
+    },
+    {
+      series: "Pale",
+      entry: "3",
+      title: "Out on a Limb",
+      author: "Wildbow",
+      words: 101423,
+      size: "medium"
+    },
+    {
+      series: "Pale",
+      entry: "4",
+      title: "Leaving a Mark",
+      author: "Wildbow",
+      words: 107693,
+      size: "medium"
+    },
+    {
+      series: "Pale",
+      entry: "5",
+      title: "Back Away",
+      author: "Wildbow",
+      words: 92991,
+      size: "medium"
+    },
+    {
+      series: "Pale",
+      entry: "6",
+      title: "Cutting Class",
+      author: "Wildbow",
+      words: 95216,
+      size: "medium"
+    },
+    {
+      series: "Pale",
+      entry: "7",
+      title: "Gone Ahead",
+      author: "Wildbow",
+      words: 108159,
+      size: "medium"
+    },
+    {
+      series: "Pale",
+      entry: "8",
+      title: "Vanishing Points",
+      author: "Wildbow",
+      words: 88332,
+      size: "medium"
+    },
+    {
+      series: "Pale",
+      entry: "9",
+      title: "Shaking Hands",
+      author: "Wildbow",
+      words: 123889,
+      size: "medium"
+    },
+    {
+      series: "Pale",
+      entry: "10",
+      title: "One After Another",
+      author: "Wildbow",
+      words: 114061,
+      size: "medium"
+    },
+    {
+      series: "Pale",
+      entry: "11",
+      title: "Dash to Pieces",
+      author: "Wildbow",
+      words: 150250,
+      size: "large"
+    },
+    {
+      series: "Pale",
+      entry: "12",
+      title: "False Moves",
+      author: "Wildbow",
+      words: 147903,
+      size: "medium"
+    },
+    {
+      series: "Pale",
+      entry: "13",
+      title: "Summer Break",
+      author: "Wildbow",
+      words: 241727,
+      size: "large"
+    },
+    {
+      series: "Pale",
+      entry: "14",
+      title: "Fall Out",
+      author: "Wildbow",
+      words: 93479,
+      size: "medium"
+    },
+    {
+      series: "Pale",
+      entry: "15",
+      title: "Playing a Part",
+      author: "Wildbow",
+      words: 162816,
+      size: "large"
+    },
+    {
+      series: "Pale",
+      entry: "16",
+      title: "Left in the Dust",
+      author: "Wildbow",
+      words: 145829,
+      size: "medium"
+    },
+    {
+      series: "Pale",
+      entry: "17",
+      title: "Gone and Done It",
+      author: "Wildbow",
+      words: 243732,
+      size: "large"
+    },
+    {
+      series: "Pale",
+      entry: "18",
+      title: "Wild Abandon",
+      author: "Wildbow",
+      words: 180415,
+      size: "large"
+    },
+    {
+      series: "Pale",
+      entry: "19",
+      title: "Crossed with Silver",
+      author: "Wildbow",
+      words: 206652,
+      size: "large"
+    },
+    {
+      series: "Pale",
+      entry: "20",
+      title: "Let Slip",
+      author: "Wildbow",
+      words: 224771,
+      size: "large"
+    },
+    {
+      series: "Pale",
+      entry: "21",
+      title: "In Absentia",
+      author: "Wildbow",
+      words: 175882,
+      size: "large"
+    },
+    {
+      series: "Pale",
+      entry: "22",
+      title: "Hard Pass",
+      author: "Wildbow",
+      words: 106606,
+      size: "medium"
+    },
+    {
+      series: "Pale",
+      entry: "23",
+      title: "Go For the Throat",
+      author: "Wildbow",
+      words: 207448,
+      size: "large"
+    },
+    {
+      series: "Pale",
+      entry: "24",
+      title: "Finish Off",
+      author: "Wildbow",
+      words: 252354,
+      size: "large"
+    },
+    {
+      series: "Pale",
+      entry: "e",
+      title: "Loose Ends",
+      author: "Wildbow",
+      words: 86786,
+      size: "medium"
+    },
+  ],
+  "claw": [
+    {
+      series: "Claw",
+      entry: "1",
+      title: "The Point",
+      author: "Wildbow",
+      words: 49579,
+      size: "small"
+    },
+    {
+      series: "Claw",
+      entry: "2",
+      title: "Retraction",
+      author: "Wildbow",
+      words: 40295,
+      size: "small"
+    },
+    {
+      series: "Claw",
+      entry: "3",
+      title: "Scrape",
+      author: "Wildbow",
+      words: 45288,
+      size: "small"
+    },
+    {
+      series: "Claw",
+      entry: "4",
+      title: "Tip",
+      author: "Wildbow",
+      words: 48293,
+      size: "small"
+    },
+    {
+      series: "Claw",
+      entry: "5",
+      title: "The Quick",
+      author: "Wildbow",
+      words: 64113,
+      size: "medium"
+    },
+    {
+      series: "Claw",
+      entry: "6",
+      title: "Bear",
+      author: "Wildbow",
+      words: 55133,
+      size: "small"
+    },
+  ],
+}
