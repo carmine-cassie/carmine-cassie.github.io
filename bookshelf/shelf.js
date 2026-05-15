be2c4bf3d55102b7472d010402d63c5ccdee5998
carmine-cassie <cassthepurple@gmail.com>
2026-05-15 21:41:24 +1000

    add bookshelf

---
+++ bookshelf/shelf.js
@@ -0,0 +1,41 @@
+const shelves = {
+  "May 2026": [
+    {
+      series: "Animorphs",
+      entry: "1",
+      title: "The Invasion",
+      author: "K.A. Applegate",
+      words: 34551,
+      size: "small",
+      bg: "#fbfcff",
+      fg: "#352fb1",
+    },
+    {
+      series: "This Used to be <br> About Dungeons",
+      entry: "1",
+      author: "Alexander Wales",
+      words: 204736,
+      size: "large",
+      bg: "#17271c",
+      fg: "#f1e666"
+    },
+    {
+      title: "The Disaster Artist",
+      author: "Greg Sestero & Tom Bissell",
+      words: 76697,
+      size: "medium",
+      bg: "#f13f17",
+      fg: "#f9fcf6"
+    },
+    {
+      series: "Arc of a Scythe",
+      entry: "1",
+      title: "Scythe",
+      author: "Neal Shusterman",
+      words: 94933,
+      size: "medium",
+      bg: "#f6ebbe",
+      fg: "#a6130c",
+    },
+  ]
+}
---
+++ bookshelf/shelf_gen.js
@@ -0,0 +1,87 @@
+// Word counts calculated with https://pengowray.github.io/word-count-epub/
+
+  let randoms = [];
+  let value = 67.67;
+  const phi = 1.618;
+  const spread = 35;
+  for (let i = 0; i < 100; i++) {
+    value = (phi * value);
+    randoms.push(Math.round((value % spread) - (spread/2)))
+  }
+
+  console.log(randoms)
+
+  const SMALL = 250*250
+  const MEDIUM = 300*300
+  const LARGE = 350*350
+
+  const shelves_div = document.getElementById("shelves");
+  
+  Object.keys(shelves).forEach((key) => {
+    shelves_div.innerHTML += `<div id="${key}" class="shelf"></div>`
+
+    let container = document.getElementById(key)
+
+    container.innerHTML += `<h2>${key}</h2>`
+
+    let books = shelves[key]
+
+    for (let i = 0; i < books.length ; i++) {
+      book = books[i]
+
+      spine_text = '';
+
+      if (book.series) {
+        spine_text += book.series;
+        if (book.entry) {
+          spine_text += ' #' + book.entry
+        }
+        if (book.title) {
+          spine_text += ': ' + book.title
+        }
+      } else {
+        spine_text = book.title
+      }
+
+      // Spine widths are thus:
+      // We say a book's word count is directly proportional its volume
+      // We'll say that a 15k word book shall be 10 x 250 pixels
+      // Volume is proportional to the square of the spine width to we can say this as:
+      // height = (words * 40) / (width ^ 2)
+
+      width_coefficient = SMALL;
+      if (book.size == "medium") {
+        width_coefficient = MEDIUM;
+      }
+      if (book.size == "large") {
+        width_coefficient = LARGE;
+      }
+
+      height = Math.ceil(book.words * 40 / width_coefficient);
+
+      bg = "#ffffff"
+      fg = "#000000"
+
+      if (book.bg) {
+        bg = book.bg
+      }
+      if (book.fg) {
+        fg = book.fg
+      }
+
+      container.innerHTML += `
+      <div class="book ${book.size}" style="height: ${height}px;left:${randoms[i % randoms.length]}px;--fg:${fg};--bg:${bg}">
+        <span class="spine title">${spine_text}</span>
+        <span class="spine">&nbsp;</span>
+        <span class="spine">${book.author}</span>
+      </div>
+    `;
+
+    }
+
+    // Add bottom of Shelf
+    container.innerHTML += `
+      <div class="book large" style="height: 3px;background-color:black"></div>
+    `;
+
+  })
