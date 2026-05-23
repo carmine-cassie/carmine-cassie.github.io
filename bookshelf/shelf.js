0bf7379dcb44fe448c414acc4c5283ec1bf6fe97
carmine-cassie <carmine-cassie@users.noreply.github.com>
2026-05-23 10:00:29 +0000

    deploy: ca2698b098cbb170813088ef7eeabd66c0e073bb

--- bookshelf/shelf.js
+++ bookshelf/shelf.js
@@ -1,25 +1,14 @@
+// Word counts calculated with https://pengowray.github.io/word-count-epub/
+
 const shelves = {
   "May 2026": [
     {
-      series: "Animorphs",
-      entry: "1",
-      title: "The Invasion",
-      author: "K.A. Applegate",
-      words: 34551,
-      size: "small",
-    },
-    {
-      series: "This Used to be <br> About Dungeons",
-      entry: "1",
-      author: "Alexander Wales",
-      words: 204736,
+      series: "Twig",
+      entry: "14",
+      title: "Thicker than Water",
+      author: "Wildbow",
+      words: 99358,
       size: "large",
-    },
-    {
-      title: "The Disaster Artist",
-      author: "Greg Sestero & Tom Bissell",
-      words: 76697,
-      size: "medium",
     },
     {
       series: "Arc of a Scythe",
@@ -29,5 +18,26 @@ const shelves = {
       words: 94933,
       size: "medium",
     },
-  ]
+    {
+      title: "The Disaster Artist",
+      author: "Greg Sestero & Tom Bissell",
+      words: 76697,
+      size: "medium",
+    },
+    {
+      series: "This Used to be <br> About Dungeons",
+      entry: "1",
+      author: "Alexander Wales",
+      words: 204736,
+      size: "large",
+    },
+    {
+      series: "Animorphs",
+      entry: "1",
+      title: "The Invasion",
+      author: "K.A. Applegate",
+      words: 34551,
+      size: "small",
+    },
+  ],
 }
