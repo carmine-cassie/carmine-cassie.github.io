0b1a284dc91412c05ef30f53a9bb5eca99b07a98
carmine-cassie <carmine-cassie@users.noreply.github.com>
2026-06-11 08:32:18 +0000

    deploy: e3ae88f9038da95272075dab5224028766ffdbcf

--- payphones/shader.js
+++ payphones/shader.js
@@ -1,4 +1,21 @@
 import * as THREE from 'three';
+import { MapControls } from 'three/addons/controls/MapControls.js';
+
+function project(lon, lat) {
+	const lon_deg = lon
+	const lat_rad = lat / 180 * Math.PI
+
+	// We use a "default zoom" of 8, which just means our values will be between 0-256
+	const n = Math.pow(2, 8)
+
+	// Based on psuedocode from https://wiki.openstreetmap.org/wiki/Slippy_map_tilenames
+	const xtile = n * ((lon_deg + 180) / 360)
+	
+	// const ytile = n * (1 - (log(tan(lat_rad) + sec(lat_rad)) / π)) / 2
+	const ytile = -n*(1 - ((Math.log(Math.tan(lat_rad) + (1/Math.cos(lat_rad))))/Math.PI))/2
+
+	return [xtile, ytile]
+}
 
 // Get the canvas from the html document
 const canvas = document.querySelector('#bgcanvas')
@@ -8,11 +25,10 @@ const scene = new THREE.Scene();
 const camera = new THREE.OrthographicCamera();
 camera.position.set(0, 0, 5);
 
-// todo add camera controls lol
 
-let x = 151.21;
-let y = -33.86778;
-let zoom = 0.32;
+let x = 235.5;
+let y = -153.6;
+let zoom = 1;
 // let zoom = 0.1;
 
 // let x = 135;
@@ -24,6 +40,20 @@ const renderer = new THREE.WebGLRenderer({canvas:canvas, alpha:true, antialias: 
 // renderer.setPixelRatio( window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
 
+const controls = new MapControls(camera, renderer.domElement);
+controls.mouseButtons = {
+	LEFT: THREE.MOUSE.PAN
+}
+controls.touches = {
+	ONE: THREE.TOUCH.PAN
+}
+controls.screenSpacePanning = true;
+controls.enableRotate = false;
+controls.enableDamping = true;
+controls.dampingFactor = 0.15;
+controls.zoomToCursor = true;
+
+
 const players = payphones["players"];
 const phones = payphones["payphones"];
 
@@ -33,7 +63,11 @@ const me = 3209;
 const my_phones = past_captures["payphoneIds"]
 const phone_coords = triangulation["coords"]
 
+
 // todo, map through the phone coords here, at the start of the doc, and project them into mercator
+Object.keys(phone_coords).forEach((id) => {
+	phone_coords[id] = project(phone_coords[id][0], phone_coords[id][1])
+})
 
 const phone_ids = Object.keys(phone_coords)
 
@@ -72,14 +106,14 @@ for (let i = 0; i < unexplored_coords.length; i++) {
 // Combine those positions into a point cloud, and add it to the scene
 const e_geometry = new THREE.BufferGeometry();
 e_geometry.setAttribute('position', new THREE.BufferAttribute(e_positions, 3));
-const e_material = new THREE.PointsMaterial({color: 0xaaaaaa, size: 1.5})
+const e_material = new THREE.PointsMaterial({color: 0xffffff, size: 1.5})
 const e_points = new THREE.Points(e_geometry, e_material)
+e_points.translateZ(3);
 scene.add(e_points)
 
 const u__geometry = new THREE.BufferGeometry();
 u__geometry.setAttribute('position', new THREE.BufferAttribute(u_positions, 3));
-// const u_material = new THREE.PointsMaterial({color: 0x332626, size: 1.5})
-const u_material = new THREE.PointsMaterial({color: 0x999999, size: 1.5})
+const u_material = new THREE.PointsMaterial({color: 0x777777, size: 1.5})
 const u_points = new THREE.Points(u__geometry, u_material)
 u_points.translateZ(2)
 scene.add(u_points)
@@ -110,7 +144,7 @@ for (let i = 0; i < phone_ids.length; i++) {
 	colors[i4 + 0] = 0.6
 	colors[i4 + 1] = 0.6
 	colors[i4 + 2] = 0.6
-	colors[i4 + 3] = (1 - explored) * 0.95
+	colors[i4 + 3] = (1 - explored)
 }
 
 let triangles = triangulation["triangles"]
@@ -128,7 +162,7 @@ fog_geometry.setIndex(tris);
 fog_geometry.setAttribute('position', new THREE.BufferAttribute(positions, 3));
 fog_geometry.setAttribute('color', new THREE.BufferAttribute(colors, 4));
 const fog_material = new THREE.MeshBasicMaterial({vertexColors: true, transparent: true, side: THREE.DoubleSide})
-const wireframe_material = new THREE.MeshBasicMaterial({vertexColors: true, transparent: true, color: 0xeeeeee, wireframe: true})
+const wireframe_material = new THREE.MeshBasicMaterial({vertexColors: true, transparent: true, color: 0xdddddd, wireframe: true})
 const fog = new THREE.Mesh(fog_geometry, fog_material)
 scene.add(fog)
 const wireframe = new THREE.Mesh(fog_geometry, wireframe_material)
@@ -142,17 +176,10 @@ function height(z, y) {
 	return height;
 }
 
-console.log(height(12, 500))
-console.log(height(12, 1500))
-console.log(height(12, 2500))
-console.log(height(12, 3500))
-
 function draw_tile(x, y, z) {
-	let n = Math.pow(2, z);
-	let width = (1/n * 360.0);
-	let lon = x / n * 360.0 - 180.0
-	let lat = Math.atan(Math.sinh(Math.PI * (1 - 2 * y / n))) * 180.0 / Math.PI
-	let height = (Math.atan(Math.sinh(Math.PI * (1 - 2 * (y - 1) / n))) * 180.0 / Math.PI) - lat;
+	let z_dif = Math.pow(2, z - 8)
+	let x_pos = (x + 0.5) / z_dif;
+	let y_pos = (y + 0.5) / z_dif;
 
 	const textureLoader = new THREE.TextureLoader();
 	const spriteTexture = textureLoader.load(`https://tile.openstreetmap.org/${z}/${x}/${y}.png`);
@@ -161,24 +188,17 @@ function draw_tile(x, y, z) {
 		color: 0xffffff 
 	});
 	const sprite = new THREE.Sprite(spriteMaterial);
-	sprite.scale.set(width, height, 1);
-	sprite.position.set(lon + width/2, lat - height/2, -30 + z);
+	sprite.scale.set(1/z_dif, 1/z_dif, 1);
+	sprite.position.set(x_pos, -y_pos, -30 + z);
+
 	scene.add(sprite)
 }
-
-// draw_tile(235, 153, 8)
-// draw_tile(3768, 2457, 12)
 
 for (let lat = 3766*1; lat <= 3770*1; lat++) {
 	for (let lon = 2455*1; lon <= 2459*1; lon++) {
 		draw_tile(lat, lon, 12)
 	}
 }
-
-// draw_tile(1883, 1228, 11)
-// draw_tile(1884, 1228, 11)
-// draw_tile(1883, 1229, 11)
-// draw_tile(1884, 1229, 11)
 
 // Update the size on window resize
 function onWindowResize(){
@@ -201,7 +221,7 @@ function animate () {
 
   requestAnimationFrame(animate)
 
-//   points.rotation.y += 0.002
+  controls.update()
 
   renderer.render(scene, camera)
 
