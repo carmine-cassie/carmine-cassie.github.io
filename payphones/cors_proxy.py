8697b5e3b7ec3a3db9f06be360996716731fddae
carmine-cassie <cassthepurple@gmail.com>
2026-06-13 14:51:17 +1000

    payphones: implement cors proxy

---
+++ payphones/cors_proxy.py
@@ -0,0 +1,28 @@
+# Because the payphones API doesn't allow Cross-Origin requests, we have this script!
+# It'll run a couple times a day with a Github workflow
+# It downloads and commits all the api files we'll need.
+
+import requests
+import json
+
+payphones = requests.get("https://payphonetag.com/api/payphones")
+if payphones.status_code == 200:
+    with open("payphones/payphones.js", "wb") as file:
+        file.write("payphones = ".encode())
+        file.write(payphones.content)
+
+    payphone_values = json.loads(payphones.content)
+    
+    for player_id in payphone_values["players"].keys():
+        captures = requests.get(f"https://payphonetag.com/api/player/{player_id}/past-captures")
+        if captures.status_code == 200:
+            with open(f"payphones/past_captures/{player_id}.json", "wb") as file:
+                file.write(captures.content)
+
+triangulation = requests.get("https://payphonetag.com/api/triangulation")
+if triangulation.status_code == 200:
+    with open("triangulation.js", "wb") as file:
+        file.write("payphones/triangulation = ".encode())
+        file.write(triangulation.content)
+
+print("Done!")
