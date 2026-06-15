5d3e97ae1f23aca3e815fecc8da5c20ccf25649f
carmine-cassie <cassthepurple@gmail.com>
2026-06-15 18:48:28 +1000

    prettier: prettify everything

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,19 +1,19 @@
 // Adapted from https://dev.to/whitep4nth3r/the-best-lightdark-mode-theme-toggle-in-javascript-368f
 
-const theme_button = document.querySelector("[data-theme-toggle]");
-let theme = localStorage.getItem("theme");
+const theme_button = document.querySelector('[data-theme-toggle]');
+let theme = localStorage.getItem('theme');
 
 if (theme === null) {
-  theme = "light";
+  theme = 'light';
 }
 
-document.querySelector("html").setAttribute("data-theme", theme);
+document.querySelector('html').setAttribute('data-theme', theme);
 
-theme_button.addEventListener("click", () => {
-  const newTheme = theme === "dark" ? "light" : "dark";
+theme_button.addEventListener('click', () => {
+  const newTheme = theme === 'dark' ? 'light' : 'dark';
 
-  localStorage.setItem("theme", newTheme);
-  document.querySelector("html").setAttribute("data-theme", newTheme);
+  localStorage.setItem('theme', newTheme);
+  document.querySelector('html').setAttribute('data-theme', newTheme);
 
   theme = newTheme;
-}); 
+});
